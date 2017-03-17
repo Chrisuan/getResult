1154908893829da6d1a55f2c9cb9c52036350e02
Christian <urban_chr@yahoo.de>
2017-03-17 20:17:12 +0100

    Add Main

---
+++ getResult/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace getResult {
+    class Program {
+        static void Main(string[] args) {
+            string[] CommandLineArgs = Environment.GetCommandLineArgs();
+
+            if(CommandLineArgs.Length < 2) {
+                Console.WriteLine("Bitte Parameter angeben");
+            }
+
+            else if(CommandLineArgs.Length > 1) {
+                switch (CommandLineArgs[1].ToLower()) {
+                    case "yes": Console.WriteLine("Ja gedrückt");
+                        break;
+                    case "no": Console.WriteLine("Nein gedrückt");
+                        break;
+                    default: Console.WriteLine("Ungültiger Parameter");
+                        break;
+                }                
+            }
+            Console.ReadLine();
+        }
+    }
+}
